3de559ec62cb7ea0d0c506a983249e8965229aa1
kio <un19451024@gmail.com>
2021-10-04 23:40:07 +0900

    Feat: vieDidLoad() 초기코드 설정 및 buttonPressed() 내용 추가

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,18 +10,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
     
+    
+    // 앱의 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mainLabel.text = "방가방가"
+        mainLabel.backgroundColor = UIColor.yellow
     }
 
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
-        // mainLabel은 모든 걸 포함하고 있기 때문에 직접 넣는 건 아니다. 하위 속성을 지정하고 넣어야 한다.
+    // mainLabel은 모든 걸 포함하고 있기 때문에 직접 넣는 건 아니다. 하위 속성을 지정하고 넣어야 한다.
         mainLabel.text = "안녕하세요"
-//        mainLabel.backgroundColor = UIColor.yellow
-        mainLabel.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        mainLabel.backgroundColor = UIColor.blue
+//        mainLabel.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
         mainLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         mainLabel.textAlignment = NSTextAlignment.right
+        
+        myButton.backgroundColor = UIColor.yellow
+        myButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
     }
 }
